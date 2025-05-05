a7ab05d32bb7eb5079aebff93bd2c8671efb295b
Ayush sen <senayush920@gmail.com>
2025-05-05 23:22:01 +0530

    add protected middlewares

---
+++ backend/middleware/userAuthMddleware.js
@@ -0,0 +1,45 @@
+const userModel = require('../models/userModel')
+const verifyToken = require('../utills/jwt')
+
+exports.protect = async(req,res,next)=>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).json({
+            message:"Authorization is missing"
+        })
+    }
+
+    if(!authHeader.startsWith('Bearer ')){
+        return res.status(401).json({
+            message:"Token must be Bearer token"
+        })
+    }
+
+    const token = authHeader.split(' ')[1]
+    if(!token){
+        return res.status(401).json({
+            message:"Token is not provided"
+        })
+    }
+
+    //verify token
+
+    const decoded = verifyToken(token);
+    if(!decoded){
+        return res.status(401).json({
+            message:"Invalid or expired token"
+        })
+    }
+
+    const user = await userModel.findById(decoded.id)
+
+    if(!user){
+        res.status(401).json({
+            message:"User not found"
+        })
+    }
+
+    req.user = user;
+    next()
+}
